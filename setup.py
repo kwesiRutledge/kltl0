2581c0eedd4a52e65cfada4efcd8289030b65895
Kwesi Rutledge <kwesir@mit.edu>
2023-05-25 10:05:21 -0400

    Added setup.py to make the project pip-installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+setup.py
+Description:
+    A setup.py file used to make this package pip-installable.
+    Taken from https://goodresearch.dev/setup.html
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='kltl0',
+    packages=find_packages(),
+)
