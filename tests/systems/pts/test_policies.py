7163772b7bceff222a80db3f915c6fe1cd30a25b
d-gaillard <114314125+d-gaillard@users.noreply.github.com>
2023-07-20 10:42:50 -0400

    Updated control policy testing include noise with sadra_noise.py.

--- tests/systems/pts/test_policies.py
+++ tests/systems/pts/test_policies.py
@@ -11,10 +11,10 @@ sadra = get_sadra_system()
 initial_traj_sadra = sadra.I*2
 
 policies = ControlPolicies(1)
-policy1 = policies.control_policy_1
-policy2 = policies.control_policy_2
+# policy1 = policies.control_policy_1
+# policy2 = policies.control_policy_2
 policy3 = policies.control_policy_3
 
-print(get_all_trajectories(policy1, 10, sadra))
-print(get_all_trajectories(policy2, 10, sadra))
-print(get_all_trajectories(policy3, 10, sadra))
+# print(get_all_trajectories(policy1, 10, sadra))
+# print(get_all_trajectories(policy2, 10, sadra))
+print(get_all_trajectories(policy3, 50, sadra))
